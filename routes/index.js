194b541224b3100092f4aa28eaa4c9839f2c9719
FrodoPano <frodopano@gmail.com>
2025-09-25 09:05:23 -0600

    login function

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const passport = require('passport');
+
 const router = require('express').Router();
 
 router.use('/', require('./swagger'));
@@ -7,6 +9,17 @@ router.get('/', (req, res) => {
 });
 
 router.use('/users', require('./users'));
+
+
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) {return next(err); }
+        res.redirect('/');
+    });
+});
 
 
 module.exports = router;
