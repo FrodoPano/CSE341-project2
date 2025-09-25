e617ace1945bc06bf54395d5659b7ab72e9977d9
FrodoPano <frodopano@gmail.com>
2025-09-25 10:47:34 -0600

    testing

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,6 @@ const router = require('express').Router();
 
 router.use('/', require('./swagger'));
 
-router.get('/', (req, res) => {
-    res.send('Hello Trainer');
-});
 
 router.use('/users', require('./users'));
 
